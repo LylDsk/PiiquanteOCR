cc41f8fc89f959e0e9e9fc73c690f79f80b2f720
Lylian Desquilbet <lylian.desquilbet@gmail.com>
2023-03-16 18:45:13 +0100

    ajout des fichiers + début login

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const app = express();
+
+const mongoose = require("mongoose");
+
+const userRoutes = require("./routes/user");
+
+var cors = require("cors");
+app.use(
+  cors({
+    origin: ["http://localhost:4200", "http://127.0.0.1:4200"],
+    credentials: true,
+  })
+);
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", true);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  next();
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@cluster0.su4aqcm.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
