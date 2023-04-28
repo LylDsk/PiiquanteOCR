59c7a932a9702a747c5d658f7ddf6f2c9c9e6648
Lylian Desquilbet <lylian.desquilbet@gmail.com>
2023-04-28 18:18:28 +0200

    Optimisation des compteurs like dans la base de données

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -9,33 +9,26 @@ exports.getAll = (req, res, next) => {
 exports.getOne = (req, res, next) => {
   console.log(req.params.id);
   Sauce.findOne({ _id: req.params.id })
-    .then((sauce) => res.status(200).json(sauce))
+    .then((sauce) => {
+      if (!sauce) {
+        return res.status(404).send(new Error("Sauce introuvable !"));
+      }
+
+      const likes = sauce.usersLiked.length;
+      const dislikes = sauce.usersDisliked.length;
+
+      const sauceData = {
+        ...sauce._doc,
+        likes,
+        dislikes,
+      };
+
+      res.status(200).json(sauceData);
+    })
     .catch((error) => res.status(404).json({ error }));
 };
 
 exports.create = (req, res, next) => {
-  /*  namesauce = JSON.parse(req.body.sauce.name);
-  console.log(namesauce);
-  console.log(req.body.sauce.manufacturer);
-  const sauce = new Sauce({
-    //email: req.body.email,
-    name: req.body.name,
-    //name: "Tabasco",
-    manufacturer: req.body.manufacturer,
-    //manufacturer: "TbscINC",
-    description: req.body.description,
-    //description: "ça pique",
-    image: `${req.protocol}://${req.get("host")}/assets/images/${
-      req.file.filename
-    }`,
-    //image: "image",
-    mainPepper: req.body.mainPepper,
-    //mainPepper: "Jalapeno",
-    heat: req.body.heat,
-    //heat: 3,
-    heatValue: req.body.heatValue,
-    //heatValue: 3,
-  });*/
   const sauceForm = JSON.parse(req.body.sauce);
   delete sauceForm._id;
   const sauce = new Sauce({
@@ -45,8 +38,6 @@ exports.create = (req, res, next) => {
     }`,
     usersLiked: [],
     usersDisliked: [],
-    likes: 0,
-    dislikes: 0,
   });
   console.log(sauce);
   sauce
@@ -55,7 +46,44 @@ exports.create = (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 };
 
-exports.like = (req, res, next) => {};
+exports.like = (req, res, next) => {
+  const userId = req.body.userId;
+  const likeValue = req.body.like;
+
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      if (!sauce) {
+        return res.status(404).send(new Error("Sauce introuvable !"));
+      }
+
+      const usersLiked = sauce.usersLiked;
+      const usersDisliked = sauce.usersDisliked;
+
+      if (likeValue === 0) {
+        usersLiked.splice(usersLiked.indexOf(userId), 1);
+        usersDisliked.splice(usersDisliked.indexOf(userId), 1);
+      } else if (likeValue === 1) {
+        if (!usersLiked.includes(userId)) {
+          usersLiked.push(userId);
+        }
+      } else if (likeValue === -1) {
+        if (!usersDisliked.includes(userId)) {
+          usersDisliked.push(userId);
+        }
+      } else {
+        return res.status(400).json({ error: "Valeur de like invalide !" });
+      }
+
+      sauce.usersLiked = usersLiked;
+      sauce.usersDisliked = usersDisliked;
+
+      sauce
+        .save()
+        .then(() => res.status(200).json({ message: "Like mis à jour !" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(404).json({ error }));
+};
 
 exports.edit = (req, res, next) => {
   Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
